77d17877970bbce3b39ce55b5a92bedeaf9ba952
Payal0000007 <payal.rana425@gmail.com>
2024-04-18 10:32:00 +0530

    new changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import '../assets/CSS/Header.css';
 import '../assets/CSS/login.css';
-
+import Image from '../assets/images/Group 427320445.png'
 import { Button, Modal } from 'react-bootstrap';
 function Header() {
   const [show, setShow] = React.useState(false);
@@ -11,7 +11,8 @@ function Header() {
   return (<>
     <div className="header">
       <div className="header-left">
-        <div className="horizontal-text">ECB</div>
+      <img src={Image} alt="Image" style={{ height: '60px', width: '107px'   }} />
+
         <div className="vertical-text">
 
           EVERYONE<br />
@@ -51,19 +52,15 @@ function Header() {
       <Modal className='login-modal1'  show={show} onHide={handleClose}style={{  padding: '2rem', borderRadius: '40px' }} >
         <>
           <div className="header-left">
-            <div className="horizontal-text">ECB</div>
-            <div className="vertical-text">
-              EVERYONE<br />
-              CAN<br />
-              BORROW
-            </div>
+          <img src={Image} alt="Image" style={{ maxHeight: '300px', marginLeft: '1rem' }} />
+
           </div>
           <div className="login-container">
-            <input type="email" className="input-field email" placeholder="Email Id" />
+            <input type="email" className="input-field email"  placeholder="Email Id" />
             <input type="password" className="input-field password" placeholder="Password" />
             <a href="#" className="forgot-password">Forgot Password?</a>
             <label className="remember-me">Remember Me?</label>
-            <button className="login-button">Login</button>
+            <button className="login-button text-white">Login</button>
           </div>
         </>
 
