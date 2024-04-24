85931be0d9f3a53ca2c6e4d87bfebb2337c896ed
developerShaina <qacsteam@gmail.com>
2024-04-24 12:32:14 +0530

    changes

--- src/Design/Layout.jsx
+++ src/Design/Layout.jsx
@@ -2,20 +2,13 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../Components/Home';
 
-
-
-
-
-
 function Layout() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        <Route path='/' element={<Home/>}/>
-    
-
-        </Routes>
+          <Route path='/' element={<Home />} />
+          </Routes>
       </BrowserRouter>
 
     </div>
