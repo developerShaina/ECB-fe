9d2b6d49d4279508eefcb88c83fdf9e1edae7c08
developerShaina <qacsteam@gmail.com>
2024-04-24 16:52:34 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import alias from '@rollup/plugin-alias'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/'
-})
+  build: {
+    rollupOptions: {
+      plugins: [
+        react(),
+        alias({
+          entries: [
+            { find: '../assets/images', replacement: './src/assets/images' }
+          ]
+        })
+      ]
+    },
+  },
+});
